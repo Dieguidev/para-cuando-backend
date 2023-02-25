b29e8589bad179312b48728e8bb631062480a5b5
robert <robert_lca91@hotmail.com>
2023-02-24 21:04:00 -0500

    swagger

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { getAllUsers, getUser, updateUser } = require('../controllers/users.controller');
 const router = express.Router();
 
+
+
 router.get('/', getAllUsers)
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
