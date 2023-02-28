2fa7b4b362d1f0e014f3796b77c16ebf875f1f8a
Dieguidev <diegogaraycullas@gmail.com>
2023-02-28 09:34:13 -0500

    se realizo segundo branch

--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -5,6 +5,62 @@ const router = express.Router();
 
 router.get('/', getAllRoles)
 
-
-
 module.exports = router;
+/**
+ * @openapi
+ * /api/v1/orders/{id}:
+ *   get:
+ *     security:
+ *       - bearerAuth: [ ]
+ *     summary: get products in cart
+ *     tags:
+ *       - Roles
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           minimun: 1
+ *         description: user id
+ *     responses:
+ *       200:
+ *         description: get products in order
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example:
+ *                     id: 2
+ *                     username: Diego
+ *                     email: diego@gmail.com
+ *                     orders:
+ *                       id: 1
+ *                       total_price: 4000
+ *                       purchase_delivered: false
+ *                       product_in_orders:
+ *                         id: 1
+ *                         quantity: 2
+ *                         price: 4000
+ *                         purchase_completed: false,
+ *                         product:
+ *                           id: 1
+ *                           name: TV
+ *                           price: 2000
+ *                           available_qty: 7
+ *                           is_available: true
+ *                           seller_id: 1
+ *       400:
+ *         description: Validation error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: validation error
+ */
